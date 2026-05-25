3b2b9560ca31ee4d2f787c0a001fd2acb6673ff2
Anuj Kumar-9 <anujkumaryadav2000.01@gmail.com>
2026-05-25 22:50:37 +0530

    aws commit

---
+++ aws.py
@@ -0,0 +1,32 @@
+print('''Aws   iam   (amazon-web-services - identity Access & management)
+
+
+IAM IS the security backbone of aws
+who can access, what the can do.
+iam free service
+
+
+
+Core components:
+IAM has 4 main component:
+
+1.USERS.
+2.GROUPS.
+3.ROLES.4.POLICIES.
+
+           1.awsmanages policy
+           2.customer-manages policy
+           3. inline policy
+           4. resource-based policy
+           5. permission boundaries(guardrail)
+           6.services control policies (aws organization)
+
+
+
+
+Evolution of polices and how user access resources.
+ 1. Identity-> policy attached -> evolution - > AWS API CALL
+IAM-USER -> Aws-managed -policy -> deny/allowed->allowed/blocked
+
+IAM-groups->  
+ ''')
